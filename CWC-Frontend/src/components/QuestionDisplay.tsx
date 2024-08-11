6416e1e123839e54d3e612a78f96e57e860adc6f
StefanStenStark <110032501+StefanStenStark@users.noreply.github.com>
2024-08-11 22:11:22 +0200

    Made the place of the right answer random

--- CWC-Frontend/src/components/QuestionDisplay.tsx
+++ CWC-Frontend/src/components/QuestionDisplay.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useMemo } from "react";
 import { Question } from "./QuestionFetcher";
 
 interface QuestionDisplayProps {
@@ -6,18 +6,29 @@ interface QuestionDisplayProps {
 }
 
 const QuestionDisplay: React.FC<QuestionDisplayProps> = ({ question }) => {
+  const shuffledAnswers = useMemo(() => {
+    const answers = [
+      { text: question.answerCorrect, isCorrect: true },
+      { text: question.answerWrongOne, isCorrect: false },
+      { text: question.answerWrongTwo, isCorrect: false },
+    ];
+
+    for (let i = answers.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [answers[i], answers[j]] = [answers[j], answers[i]];
+    }
+
+    return answers;
+  }, [question]);
+
   return (
     <div>
       <p>{question.questionText}</p>
-      <div>
-        <code>{question.answerCorrect}</code>
-      </div>
-      <div>
-        <code>{question.answerWrongOne}</code>
-      </div>
-      <div>
-        <code>{question.answerWrongTwo}</code>
-      </div>
+      {shuffledAnswers.map((answer, index) => (
+        <div key={index}>
+          <code>{answer.text}</code>
+        </div>
+      ))}
     </div>
   );
 };
