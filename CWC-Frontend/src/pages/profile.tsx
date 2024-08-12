c42cbee9207a52b3d4e36d87b4e466acc055dccf
StefanStenStark <110032501+StefanStenStark@users.noreply.github.com>
2024-08-12 10:04:04 +0200

    Uppdated text in profile

--- CWC-Frontend/src/pages/profile.tsx
+++ CWC-Frontend/src/pages/profile.tsx
@@ -4,7 +4,7 @@ import meMario from "../assets/meMario.png";
 function Profile() {
   return (
     <section className="center-holder">
-      <p>StefanStenStark</p>
+      <h2>StefanStenStark</h2>
       <div>
         <img
           src={meMario}
@@ -20,7 +20,7 @@ function Profile() {
         A great coder with the passion to do the coding and do the thing where
         you turn the passion into a profession.{" "}
       </p>
-      <p>One day his mother told him to hack into the mainframe. And he did!</p>
+      <p>One day Oden told him to hack into the mainframe. And he did!</p>
       <p>
         Some people say that he is as strong as a stone.. Not sure how strong a
         stone is. But he is!
@@ -29,6 +29,10 @@ function Profile() {
         He got all the highscores. Some say that he even made the first
         questions and answers.
       </p>
+      <h2>Score</h2>
+      <p>React basic: 7/7</p>
+      <p>React advanced: 12/12</p>
+      <p>React expert: 32/30</p>
     </section>
   );
 }
